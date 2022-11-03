60d90c8efaa8a7264a540afc71249e682acefe9f
jakob <jakob.elias.albrecht@uni-weimar.de>
2022-11-03 20:52:10 +0100

    changed string to character

--- app/src/main/java/de/buw/se4de/grid.java
+++ app/src/main/java/de/buw/se4de/grid.java
@@ -20,17 +20,17 @@ public class grid {
     }
 
     public void print() {
-        ArrayList<String> letters = new ArrayList<>();
-        letters.add("A");
-        letters.add("B");
-        letters.add("C");
-        letters.add("D");
-        letters.add("E");
-        letters.add("F");
-        letters.add("G");
-        letters.add("H");
-        letters.add("I");
-        letters.add("J");
+        ArrayList<Character> letters = new ArrayList<>();
+        letters.add('A');
+        letters.add('B');
+        letters.add('C');
+        letters.add('D');
+        letters.add('E');
+        letters.add('F');
+        letters.add('G');
+        letters.add('H');
+        letters.add('I');
+        letters.add('J');
 
         System.out.println("\n  0 1 2 3 4 5 6 7 8 9  ");
         for(int current_row=0; current_row < this.rows;current_row++) {
