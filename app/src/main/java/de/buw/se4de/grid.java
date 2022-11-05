846e4c512f71f5b110adbdee16eeff79375bf9fa
jakob <jakob.elias.albrecht@uni-weimar.de>
2022-11-05 20:49:25 +0100

    began placing ship method

--- app/src/main/java/de/buw/se4de/grid.java
+++ app/src/main/java/de/buw/se4de/grid.java
@@ -1,7 +1,7 @@
 package de.buw.se4de;
 
-import com.google.common.collect.Table;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class grid {
@@ -9,8 +9,9 @@ public class grid {
     int collums;
     Ship[] ships;
     cell[][] cells;
-
+    Scanner scanner = new Scanner(System.in);
     Character[] letters ={'A','B','C','D','E','F','G','H','I','J'};
+    ArrayList<Integer> shiplength= new ArrayList<>(Arrays.asList(5,4,4,3,3,2));
 
     public grid(){
         this.rows = 10;
@@ -24,13 +25,15 @@ public class grid {
             }
         }
 
+        for (int i =0;i< ships.length;i++){
+            ships[i]=new Ship();
+        }/*
         this.ships[0] = new Ship2();
         this.ships[1] = new Ship3();
         this.ships[2] = new Ship4();
         this.ships[3] = new Ship4();
         this.ships[4] = new Ship5();
-
-        Character[] letters ={'A','B','C','D','E','F','G','H','I','J'};
+        */
     }
 
     public void setSingleShip(Ship ship){
@@ -64,19 +67,17 @@ public class grid {
                 condition = true;
             }
         }
-
     }
 
-    public void setShips(){}
-
-    public void shoot(String row, int collumn){
+    public void shoot(String row, int column){
         int rowInt = searchLetters(row);
-        if (cells[rowInt][collumn].hasShip){
-            cells[rowInt][collumn].shotShip=true;
+        if (cells[rowInt][column].hasShip){
+            cells[rowInt][column].shotShip=true;
         }
-        cells[rowInt][collumn].dead=true;
+        cells[rowInt][column].dead=true;
     }
 
+    //checks if cell was shot and has a ship, so it can display the ship
     public boolean checkShip(cell cell){
         return cell.dead && cell.hasShip;
     }
@@ -84,17 +85,15 @@ public class grid {
     public void playSingleGame(){
         while (ships.length!=0){
             PrintEnemyGrid();
-            Scanner scanner = new Scanner(System.in);
 
             System.out.println("Where do you wish to shoot? Write a letter and then a Number ");
             String row = scanner.nextLine();
-            int collumn = scanner.nextInt();
-            shoot(row,collumn);
+            int column = scanner.nextInt();
+            shoot(row,column);
         }
     }
 
     public void PrintEnemyGrid() {
-
         System.out.println("\n  0 1 2 3 4 5 6 7 8 9  ");
         for(int current_row=0; current_row < this.rows;current_row++) {
             System.out.print(letters[current_row]+"|");
@@ -102,16 +101,14 @@ public class grid {
                 cell current_cell = cells[current_row][current_col];
                 //if cell has a shot ship
                 if (checkShip(current_cell)){
-                    System.out.print("= ");
-                }//if cell is dead but no ship
-
+                    System.out.print("0 ");
+                }//if cell has a ship (testing)
                 else if(current_cell.hasShip){
                     System.out.print("H ");
-                }
-
+                }//cell was shot but missed ships
                 else if(current_cell.dead){
                     System.out.print("X ");
-                }//if cell is not dead
+                }//if cell was not touched yet
                 else {
                     System.out.print("~ ");
                 }
@@ -120,7 +117,26 @@ public class grid {
         }
         System.out.println("  0 1 2 3 4 5 6 7 8 9  ");
     }
-
+    //let the enemy place one ship
+    //just hardcoding some ships
+    public void placingShip(){
+        //checking wich ship to place
+        boolean NotValidLength = true;
+        while (NotValidLength) {
+            System.out.println("You have the following ship lengths remaining: ");
+            printShipLengths();
+            System.out.println("Which ship do you want to place? (in length)");
+            int len = scanner.nextInt();
+            //checking for valid len, still need to catch non-number inputs
+            if (!lengthCheck(len)) {
+                System.out.println("Please try again with a valid length.");
+            }else {
+                NotValidLength = false;
+                shiplength.remove(len);
+            }
+        }
+        //now place the ship
+    }
     public int searchLetters(String s){
         for (int i=0;i< letters.length;++i) {
             char c = s.charAt(0);
@@ -129,5 +145,36 @@ public class grid {
             }
         }
         return -1;
+    }
+    public int searchNumbers(int i){
+        for (int j=0;j<shiplength.length;++j){
+            if (i==shiplength[j]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void remove(int[] intarray,int index){
+        int[] copy = new int[shiplength.length];
+        for (int i=0,j = 0;i<intarray.length;i++){
+            if (i!=index){
+
+            }
+            intarray[i] = intarray[i+1];
+        }
+    }
+    public boolean lengthCheck(int len){
+        for (int i : shiplength){
+            if(len==i){
+                return true;
+            }
+        }
+        return false;
+    }
+    //print remaining ship lengths
+    public void printShipLengths(){
+        for (int i:shiplength){
+            System.out.print(i+", ");
+        }
     }
 }
