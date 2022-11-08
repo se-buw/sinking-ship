00aced01d1b5c9b58b108cc5030e8c15ba1b06d3
jakob <jakob.elias.albrecht@uni-weimar.de>
2022-11-08 11:50:12 +0100

    changed game order

--- app/src/main/java/de/buw/se4de/GameEngine.java
+++ app/src/main/java/de/buw/se4de/GameEngine.java
@@ -9,14 +9,16 @@ public class GameEngine {
 
     public void standardGame(){
         Scanner scanner = new Scanner(System.in);
-        while (EnemyGrid.shiplength.size()!=0){
-            PlayerGrid.PlacingEnemyShip();
-            PlayerGrid.PrintPLayerGrid();
-        }
+        //Begin input
         while (PlayerGrid.shiplength.size()!=0){
             PlayerGrid.PlacingPlayerShip();
             PlayerGrid.PrintPLayerGrid();
         }
+
+        EnemyGrid.PlacingEnemyShip();
+        EnemyGrid.PrintEnemyGrid();
+
+        //play till one player has no ship left
         while (PlayerGrid.shiplength.size()!=0||EnemyGrid.shiplength.size()!=0){
             EnemyGrid.PrintEnemyGrid();
             System.out.println("Where do you wish to shoot? Write a letter and then a Number ");
